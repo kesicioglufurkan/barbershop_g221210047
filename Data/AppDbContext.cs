3cfdce5e85e24ce2ff28148e8cc68406a9a6d623
furka <furka@KESICIOGLU>
2024-12-10 23:22:14 +0300

    Proje dosyası ekle.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace barbershop_g221210047.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Salon> Salons { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+}
